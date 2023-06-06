e84f8fb2955c052d221aa8ef55868f31a2492aa6
ish78 <ish78@DESKTOP-N83G0DH>
2023-06-07 02:48:16 +0900

    azimuth

--- azimuth.m
+++ azimuth.m
@@ -1,7 +1,7 @@
-function az = azimuth(ENU) % ENU [ E N U ; E N U ...] ENU  = km
-    A = zeros(size(ENU,1),1);
+function az = azimuth(ENU) % ENU [ E N U ; E N U ...]nx3,  ENU  = km
+    A = zeros(1, size(ENU,1));
     for a = 1:size(ENU, 1)
-        A(a,1)= (acos(ENU(a,2)/((ENU(a,1)^2+ENU(a,2)^2)^(1/2)))) * 180 / pi;
+        A(1,a)= (acos(ENU(a,2)/((ENU(a,1)^2+ENU(a,2)^2)^(1/2)))) * 180 / pi;
     end
     az = A;
           
